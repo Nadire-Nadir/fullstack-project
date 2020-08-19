dd7f290cf0349da369dd36858a11fee03d7233d7
Nadire-Nadir <nadiraemam@gmail.com>
2020-08-19 23:40:52 +0300

    some bugs

--- client/src/components/popUps.js
+++ client/src/components/popUps.js
@@ -13,7 +13,13 @@ const PopUp = (props) => (
           <div>{props.popup_info_P01}</div>
         </div>
         <div className="popup_content_part02">
-          <div></div>
+          <div>
+            <p>A tall human tribesman strides through a blizzard, draped in fur and hefting his axe. He laughs as he charges toward the frost giant who dared poach his people’s elk herd.</p>
+            <p>A half-orc snarls at the latest challenger to her authority over their savage tribe, ready to break his neck with her bare hands as she did to the last six rivals.</p>
+            <p>Frothing at the mouth, a dwarf slams his helmet into the face of his drow foe, then turns to drive his armored elbow into the gut of another.</p>
+            <p>These barbarians, different as they might be, are defined by their rage: unbridled, unquenchable, and unthinking fury. More than a mere emotion, their anger is the ferocity of a cornered predator, the unrelenting assault of a storm, the churning turmoil of the sea.</p>
+            <p>For some, their rage springs from a communion with fierce animal spirits. Others draw from a roiling reservoir of anger at a world full of pain. For every barbarian, rage is a power that fuels not just a battle frenzy but also uncanny reflexes, resilience, and feats of strength.</p>
+          </div>
           <div>{props.popup_info_P02}</div>
         </div>
       </div>
